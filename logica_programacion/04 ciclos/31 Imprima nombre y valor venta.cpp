f302564852230b22143cc850c48c19ca9e8cb4ce
Alejandro Orozco <designifrit@gmail.com>
2020-06-07 22:51:55 -0500

    Update

---
+++ logica_programacion/04 ciclos/31 Imprima nombre y valor venta.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <io.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int PU, CV, VT;
+char NOM[41];
+char N;
+void main() {
