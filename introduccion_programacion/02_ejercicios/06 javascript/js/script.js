d0547e75f2f508a7862571c0778580adde80722d
Alejandro Orozco <designifrit@gmail.com>
2020-06-11 20:08:42 -0500

    js

--- introduccion_programacion/02_ejercicios/06 javascript/js/script.js
+++ introduccion_programacion/02_ejercicios/06 javascript/js/script.js
@@ -1,5 +1,15 @@
 
 
-let nombreUsuario;
-let password;
+let nombreUsuario='Alejandro Orozco';
+let password='admin123*';
 
+let tienda='DELI';
+let producto='Torta chocolate';
+let costo=32000;
+
+let descuento=0.2;
+
+let precioTotal=costo-(costo * descuento);
+
+console.log('El costo total es: ' + precioTotal + ' pesos');
+// alert('El costo total es: ' + precioTotal + ' pesos');
