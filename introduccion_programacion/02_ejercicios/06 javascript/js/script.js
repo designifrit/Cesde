ed6bdffe3b04a426eccee6c8591137bbe21c9435
Alejandro Orozco <designifrit@gmail.com>
2020-06-18 19:49:40 -0500

    u

--- introduccion_programacion/02_ejercicios/06 javascript/js/script.js
+++ introduccion_programacion/02_ejercicios/06 javascript/js/script.js
@@ -1,5 +1,5 @@
 
-
+/*
 let nombreUsuario='Alejandro Orozco';
 let password='admin123*';
 
@@ -13,3 +13,13 @@ let precioTotal=costo-(costo * descuento);
 
 console.log('El costo total es: ' + precioTotal + ' pesos');
 // alert('El costo total es: ' + precioTotal + ' pesos');
+*/
+
+let nav = document.getElementById('nav');
+console.log(nav);
+
+let tituloMain = document.getElementById('titulo');
+tituloMain.textContent = "Soy Anonymous";
+
+let parrafo1 = document.getElementById('parrafo1');
+parrafo1.classList.add('parrafo1script');
