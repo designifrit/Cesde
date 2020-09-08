b52ef46911c60fd5de8c930d1572d106476bef22
Alejandro Orozco <designifrit@gmail.com>
2020-09-08 18:13:13 -0500

    update

---
+++ 2do_periodo/programacion_web/02_actividades/01_introduccion_php/index.php
@@ -0,0 +1,10 @@
+<?php
+    $nombreUsuario = 'Alejandro O';     // Variable
+    define('constante',3.1415);          // Constante
+    $edad = 34;
+    $union = false;
+
+
+    echo('El nombre de usuario es '.$nombreUsuario); // Concatenación
+    echo('El número PI es '.constante);
+?>
