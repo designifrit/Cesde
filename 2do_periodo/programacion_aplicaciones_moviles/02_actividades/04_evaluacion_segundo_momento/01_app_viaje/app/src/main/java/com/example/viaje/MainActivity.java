fe97ee446c7258572b3104494f31233eee0ffb1a
Alejandro Orozco <designifrit@gmail.com>
2020-11-11 22:35:15 -0500

    Programacion movil - Taller 2do momento

---
+++ 2do_periodo/programacion_aplicaciones_moviles/02_actividades/04_evaluacion_segundo_momento/01_app_viaje/app/src/main/java/com/example/viaje/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.viaje;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    RadioButton rbAvion, rbBus, rbTaxi, rbLasagna, rbPizza, rbHamburguesa;
+    EditText etCantTickets;
+    TextView tvTotal;
+    CheckBox cbPropina;
+    Button btCalcular, btLimpiar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // getSupportActionBar().hide();
+        rbAvion = findViewById(R.id.rbAvion);
+        rbBus = findViewById(R.id.rbBus);
+        rbTaxi = findViewById(R.id.rbTaxi);
+        rbLasagna = findViewById(R.id.rbLasagna);
+        rbPizza = findViewById(R.id.rbPizza);
+        rbHamburguesa = findViewById(R.id.rbHamburguesa);
+        etCantTickets = findViewById(R.id.etCantTickets);
+        tvTotal = findViewById(R.id.tvTotal);
+        cbPropina = findViewById(R.id.cbPropina);
+        btCalcular = findViewById(R.id.btCalcular);
+        btLimpiar = findViewById(R.id.btLimpiar);
+    }
+
+    public int HallarCostoTransporte(View view){
+        int cantPersonas, medTransporte, costoTransporte;
+        cantPersonas = 0;
+        costoTransporte = 0;
+        medTransporte = 0;
+        cantPersonas = Integer.parseInt(etCantTickets.getText().toString());
+
+        if(rbAvion.isChecked()){
+            medTransporte = 210000;
+        }else if(rbBus.isChecked()) {
+            medTransporte = 75000;
+        }else if(rbTaxi.isChecked()){
+            medTransporte = 135000;
+        }
+
+        costoTransporte = medTransporte * cantPersonas;
+
+        return costoTransporte;
+    }
+
+    @SuppressLint("SetTextI18n")
+    public  int HallarCostoAlimentacion(View view){
+        int cantPersona, aliment, costAliments;
+        cantPersona = 0;
+        aliment = 0;
+        costAliments = 0;
+
+        if(rbAvion.isChecked()){
+            if(rbLasagna.isChecked()){
+                aliment = 14500;
+            }else if(rbPizza.isChecked()){
+                aliment = 11000;
+            }else if(rbHamburguesa.isChecked()){
+                aliment = 16500;
+            }
+        }
+
+        cantPersona = Integer.parseInt(etCantTickets.getText().toString());
+        costAliments = aliment * cantPersona;
+
+        return  costAliments;
+    }
+
+    public double HallarCostoPropina(View view){
+        int costTransported, costaAlimentary;
+        double valProp;
+        valProp = 0;
+        costTransported = 0;
+        costaAlimentary = 0;
+
+        costTransported = HallarCostoTransporte(view);
+        costaAlimentary = HallarCostoAlimentacion(view);
+
+        if(cbPropina.isChecked()){
+            valProp = (costTransported + costaAlimentary) * 0.10;
+        }else{
+            valProp = 0;
+        }
+
+        return valProp;
+    }
+
+    public void HallarTotal(View view){
+        double total;
+        total = 0;
+
+        total = HallarCostoTransporte(view) + HallarCostoAlimentacion(view) + HallarCostoPropina(view);
+        tvTotal.setText(String.valueOf(total));
+    }
+
+    public void Limpiar(View view){
+        rbAvion.isChecked();
+        rbBus.isChecked();
+        rbTaxi.isChecked();
+        rbLasagna.isChecked();
+        rbPizza.isChecked();
+        rbHamburguesa.isChecked();
+        cbPropina.setChecked(false);
+        etCantTickets.setText("1");
+        tvTotal.setText("");
+    }
+}
