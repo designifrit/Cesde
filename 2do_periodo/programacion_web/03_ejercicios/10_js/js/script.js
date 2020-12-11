d63c2f53448156f40b04ac39c954122d4222a5cd
Alejandro Orozco <designifrit@gmail.com>
2020-12-11 14:44:39 -0500

    Programación Movil - Concesionario

--- 2do_periodo/programacion_web/03_ejercicios/10_js/js/script.js
+++ 2do_periodo/programacion_web/03_ejercicios/10_js/js/script.js
@@ -1,4 +1,24 @@
 
+// Consumir API con JS
+// 1. Identificar la URL de la API a consumir
+let url = "https://swapi.dev/api/starships";
+
+// 2. Configurar parámetros de envío
+let parametros = {
+    method: "GET";
+};
+
+// 3. Utilizar el método FETCH
+fetch(url, parametros)
+    .then(respuesta => respuesta.json())
+    .then(datos => depurar(datos))
+
+// 4. Definir la función depurar
+function depurar(datos){
+    console.log(datos);
+}
+
+
 let titulo = document.getElementById('titulo');
 let imagen = document.getElementById('imagen');
 let audio = document.getElementById('audio');
