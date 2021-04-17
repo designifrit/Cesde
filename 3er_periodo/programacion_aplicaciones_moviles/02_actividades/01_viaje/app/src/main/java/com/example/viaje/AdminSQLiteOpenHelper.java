cb3f9bdad1d011c2f0ee4a1230ba9f7e83ddddb4
Alejandro Orozco <designifrit@gmail.com>
2021-04-17 12:06:08 -0500

    Improvements

--- 3er_periodo/programacion_aplicaciones_moviles/02_actividades/01_viaje/app/src/main/java/com/example/viaje/AdminSQLiteOpenHelper.java
+++ 3er_periodo/programacion_aplicaciones_moviles/02_actividades/01_viaje/app/src/main/java/com/example/viaje/AdminSQLiteOpenHelper.java
@@ -1,23 +1,23 @@
-//package com.example.viaje;
-//
-//import android.content.Context;
-//import android.database.sqlite.SQLiteDatabase;
-//import android.database.sqlite.SQLiteOpenHelper;
-//import android.support.annotation.Nullable;
-//
-//public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
-//    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-//        super(context, name, factory, version);
-//    }
-//
-//    @Override
-//    public void onCreate(SQLiteDatabase db) {
-//        db.execSQL("create table viaje(codigo text primary key, ciudad text, persona text, valor text, int)");
-//    }
-//
-//    @Override
-//    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-//        db.execSQL("drop table if exists viaje");
-//        db.execSQL("create table viaje(codigo text primary key, ciudad text, persona text, valor text)");
-//    }
-//}
+package com.example.viaje;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table viaje(codigo text primary key, ciudad text, persona text, valor text, anular boolean)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists viaje");
+        db.execSQL("create table viaje(codigo text primary key, ciudad text, persona text, valor text, anular boolean)");
+    }
+}
--- 3er_periodo/programacion_aplicaciones_moviles/02_actividades/01_viaje/app/src/main/java/com/example/viaje/RegistrarActivity.java
+++ 3er_periodo/programacion_aplicaciones_moviles/02_actividades/01_viaje/app/src/main/java/com/example/viaje/RegistrarActivity.java
@@ -1,6 +1,8 @@
 package com.example.viaje;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -91,7 +93,7 @@ public class RegistrarActivity extends AppCompatActivity {
             }else{
                 anular = objetoViaje.getAnular();
 
-                if(anular != false){
+                if(anular){
                     if(swAnular.isChecked()){
                         etCiudad.setText(objetoViaje.getCiudad());
                         etPersona.setText(objetoViaje.getPersona());
@@ -138,7 +140,7 @@ public class RegistrarActivity extends AppCompatActivity {
         ciudad = etCiudad.getText().toString();
         persona = etPersona.getText().toString();
         valor = etValor.getText().toString();
-        Boolean anular = false;
+        boolean anular;
 
         if(codigo.isEmpty() || ciudad.isEmpty() || persona.isEmpty() || valor.isEmpty()){
             Toast.makeText(this, "Consulte primero un registro para poder eliminarlo", Toast.LENGTH_SHORT).show();
@@ -152,6 +154,51 @@ public class RegistrarActivity extends AppCompatActivity {
                 limpiarCampos();
             }
         }
+    }
+
+    public void guardarSQL(View view){
+        
+
+
+
+
+//        MainSQLiteOpenHelper conectarDb = new MainSQLiteOpenHelper(this, "Concesionario", null, 1);
+//        SQLiteDatabase db = conectarDb.getWritableDatabase();
+//
+//        placa = etPlaca.getText().toString();
+//        modelo = etModelo.getText().toString();
+//        marca = etMarca.getText().toString();
+//        precio = etPrecio.getText().toString();
+//        clave1 = etIngPassword.getText().toString();
+//        clave2 = etVerPassword.getText().toString();
+//
+//        if(placa.isEmpty() || modelo.isEmpty() || marca.isEmpty() || precio.isEmpty() || clave1.isEmpty() || clave2.isEmpty()){
+//            Toast.makeText(this, "Todos los campos son requeridos", Toast.LENGTH_LONG).show();
+//            etPlaca.requestFocus();
+//        }else{
+//            if(!clave1.equals(clave2)){
+//                Toast.makeText(this, "La contraseña deben coincidir", Toast.LENGTH_LONG).show();
+//                etIngPassword.requestFocus();
+//            }else{
+//                ContentValues dato = new ContentValues();
+//
+//                dato.put("idPlaca", placa);
+//                dato.put("modelo", modelo);
+//                dato.put("marca", marca);
+//                dato.put("precio", precio);
+//                dato.put("password", clave1);
+//
+//                long respuesta = db.insert("usuario", null, dato);
+//
+//                if(respuesta > 0){
+//                    Toast.makeText(this, "Registro guardado", Toast.LENGTH_SHORT).show();
+//                    Limpiar(view);
+//                }else{
+//                    Toast.makeText(this, "Error al guardar el registro", Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        }
+//        db.close();
     }
 
     public void limpiar(View view){
